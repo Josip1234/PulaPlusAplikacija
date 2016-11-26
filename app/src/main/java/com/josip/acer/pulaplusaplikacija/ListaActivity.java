b538f63640201271cb17b592626cb6778907ee64
Josip Bošnjak <jbosnjak@unipu.hr>
2016-11-26 11:10:34 +0100

    izbrisani fragmenti napravljeni buttoni treba sad to urediti

--- app/src/main/java/com/josip/acer/pulaplusaplikacija/ListaActivity.java
+++ app/src/main/java/com/josip/acer/pulaplusaplikacija/ListaActivity.java
@@ -1,27 +1,57 @@
 package com.josip.acer.pulaplusaplikacija;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ListaActivity extends AppCompatActivity {
-
+private Button b1;
+private Button b2;
+private Button b3;
+private Button b4;
+private Button b5;
+private Button b6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
-        LstFragment lstFragment = (LstFragment) getSupportFragmentManager().findFragmentByTag("lstfragment");
+        b1=(Button) findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Trgovine();
+            }
+        });
+        b2=(Button) findViewById(R.id.b2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dogadaj();
+            }
+        });
+        b3 = (Button) findViewById(R.id.b3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Booking();
+            }
+        });
 
-
-        if (lstFragment == null) {
-            lstFragment = new LstFragment();
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.add(android.R.id.content, lstFragment, "lstfragment");
-            transaction.commit();
-
-
-        }
 
     }
+public void Trgovine(){
+    Intent i = new Intent(this,TrgovineActivity.class);
+    startActivity(i);
+}
+public void Dogadaj(){
+        Intent p= new Intent(this,DogadjajActivity.class);
+        startActivity(p);
+        }
+public void Booking(){
+        Intent a = new Intent(this,BookingActivity.class);
+        startActivity(a);
+        }
 }
